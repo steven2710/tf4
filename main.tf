a465969584d333d747d240e51adddd6f0f668576
steven2710 <tuanhiep2710@gmail.com>
2025-08-25 17:08:03 +0700

    full commit and explation of result

--- main.tf
+++ main.tf
@@ -3,5 +3,83 @@ provider "aws" {
 }
 
 module "create_users" {
-    source = "github.com/steven2710/tf4//live/global/iam/variables.tf"
+    source = "github.com/steven2710/tf4//live/global/iam/"
+    user_names = ["Steven", "Joe", "Bush", "Peter"]
 }
+
+/*
+Tạo module có tên gọi là "create_users", với source là github.com/steven2710/tf4//live/global/iam/
+Vì trong ithub.com/steven2710/tf4//live/global/iam/variables.tf, đã khởi tạo biến là variable "user_names", nên trong module "create_users"
+chỉ cần gán user_names theo yêu cầu.
+
+hiepnt@LAP52-HiepNT:/mnt/d/Document/Terraform/LAB/LAB4$ terraform apply
+
+Terraform used the selected providers to generate the following execution plan. Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # module.create_users.aws_iam_user.example[0] will be created
+  + resource "aws_iam_user" "example" {
+      + arn           = (known after apply)
+      + force_destroy = false
+      + id            = (known after apply)
+      + name          = "Steven"
+      + path          = "/"
+      + tags_all      = (known after apply)
+      + unique_id     = (known after apply)
+    }
+
+  # module.create_users.aws_iam_user.example[1] will be created
+  + resource "aws_iam_user" "example" {
+      + arn           = (known after apply)
+      + force_destroy = false
+      + id            = (known after apply)
+      + name          = "Joe"
+      + path          = "/"
+      + tags_all      = (known after apply)
+      + unique_id     = (known after apply)
+    }
+
+  # module.create_users.aws_iam_user.example[2] will be created
+  + resource "aws_iam_user" "example" {
+      + arn           = (known after apply)
+      + force_destroy = false
+      + id            = (known after apply)
+      + name          = "Bush"
+      + path          = "/"
+      + tags_all      = (known after apply)
+      + unique_id     = (known after apply)
+    }
+
+  # module.create_users.aws_iam_user.example[3] will be created
+  + resource "aws_iam_user" "example" {
+      + arn           = (known after apply)
+      + force_destroy = false
+      + id            = (known after apply)
+      + name          = "Peter"
+      + path          = "/"
+      + tags_all      = (known after apply)
+      + unique_id     = (known after apply)
+    }
+
+Plan: 4 to add, 0 to change, 0 to destroy.
+
+Do you want to perform these actions?
+  Terraform will perform the actions described above.
+  Only 'yes' will be accepted to approve.
+
+  Enter a value: yes
+
+module.create_users.aws_iam_user.example[3]: Creating...
+module.create_users.aws_iam_user.example[2]: Creating...
+module.create_users.aws_iam_user.example[0]: Creating...
+module.create_users.aws_iam_user.example[1]: Creating...
+module.create_users.aws_iam_user.example[1]: Creation complete after 1s [id=Joe]
+module.create_users.aws_iam_user.example[2]: Creation complete after 1s [id=Bush]
+module.create_users.aws_iam_user.example[0]: Creation complete after 2s [id=Steven]
+module.create_users.aws_iam_user.example[3]: Creation complete after 2s [id=Peter]
+
+Apply complete! Resources: 4 added, 0 changed, 0 destroyed.
+hiepnt@LAP52-HiepNT:/mnt/d/Document/Terraform/LAB/LAB4$ 
+*/
