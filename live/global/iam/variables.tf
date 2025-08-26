0b980463c60ae8bde8731cb7c25801b714b00565
steven2710 <tuanhiep2710@gmail.com>
2025-08-26 13:13:34 +0700

    add

--- live/global/iam/variables.tf
+++ live/global/iam/variables.tf
@@ -5,9 +5,16 @@ default = ["neo", "trinity", "morpheus"]
 }
 
 resource "aws_iam_user" "example" {
-for_each = toset(var.user_names) #toset là để convert list thành set. for_each chỉ hỗ trợ set, map.
-#set về cơ bản giống như list, nhưng set không hỗ trợ trùng lặp, các phần tử là duy nhất
-#đối với set, thì each.key hay each.value đều như nhau. Tuy nhiên đối với map, dạng name và value
-#thì each.key là name, còn key.value là value
-name = each.key
+    for_each = toset(var.user_names) #toset là để convert list thành set. for_each chỉ hỗ trợ set, map.
+    #set về cơ bản giống như list, nhưng set không hỗ trợ trùng lặp, các phần tử là duy nhất
+    #đối với set, thì each.key hay each.value đều như nhau. Tuy nhiên đối với map, dạng name và value
+    #thì each.key là name, còn key.value là value
+    name = each.key
+}
+
+variable "user_roles" {
+  default = {
+    Steven1   = "admin"
+    Trump1     = "dev"
+  }
 }
--- main.tf
+++ main.tf
@@ -3,9 +3,26 @@ provider "aws" {
 }
 
 module "create_users" {
-    source = "github.com/steven2710/tf4//live/global/iam/?ref=v0.0.5"
-    user_names = ["Steven", "Trump"]  
+  source = "github.com/steven2710/tf4//live/global/iam/?ref=v0.0.5"
+  user_names = ["Steven1", "Trump1"]
+  
+  for_each = user_roles
+
+  name = each.key   # tên user
+  # Có thể gán role (tag chẳng hạn) từ each.value
+  tags = {
+    Role = each.value
+  }
 }
+
+
+
+
+
+
+
+
+
 # Print out all username, với create_users là tên của module, còn created_user_names là tên của output trong file outputs.tf nằm trong /live/global/iam/
 output "all_user_names" {
   value = module.create_users.created_user_names
